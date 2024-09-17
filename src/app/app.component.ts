059e881dc1a2bf004f1e1f613d5edea7d990cf8b
Tobias Muehlberger <tobias@muehlberger.dev>
2024-09-17 22:14:32 +0300

    First working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, input, signal, ViewChild } from '@angular/core';
+import parsePhoneNumber, { isValidNumber, isValidPhoneNumber, parseNumber } from 'libphonenumber-js'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'WALinker';
+  private readonly defaultCountry = "AT"
+
+  numberValid = signal<boolean|undefined>(undefined)
+
+  formattedNumber = signal<string|undefined>(undefined)
+
+  whatsappLink = signal<string|undefined>(undefined)
+
+  @ViewChild("number")
+  numberInput?: ElementRef<HTMLInputElement>
+
+  numberChange() {
+    const inputValue = this.numberInput?.nativeElement?.value
+    if (!inputValue) {
+      this.numberValid.set(undefined)
+      return
+    }
+
+    const phoneNumber = parsePhoneNumber(inputValue, this.defaultCountry)
+
+    console.log(phoneNumber)
+
+    if (!phoneNumber || !phoneNumber.isValid()) {
+      this.numberValid.set(false)
+      return
+    }
+
+    let numberStr = phoneNumber.formatInternational()
+
+    this.formattedNumber.set(numberStr)
+
+    numberStr = numberStr
+      .replaceAll(' ', '')
+      .replaceAll('+', '')
+
+    this.whatsappLink.set(`https://wa.me/${numberStr}`)
+    this.numberValid.set(true)
+  }
 }
